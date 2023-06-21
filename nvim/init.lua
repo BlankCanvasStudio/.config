c86acb8f7ae26d11e098d3d11c755efd7775e78a
adam <spencerstingley6@gmail.com>
2023-06-21 08:46:42 -0500

    Added contrast to window size bar

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,7 +13,7 @@ vim.cmd.set('expandtab')
 vim.cmd.set('tabstop=8 softtabstop=0')
 
 vim.cmd.set('colorcolumn=71')
-vim.cmd.hi('ColorColumn ctermbg=lightcyan guibg=#bb9af7')
+vim.cmd.hi('ColorColumn guifg=black guibg=#bb9af7')
 
 vim.cmd.hi('LineNr guifg=#ffffff')
 
