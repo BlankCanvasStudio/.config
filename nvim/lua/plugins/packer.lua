3d7de01d1b3e92e611fea6112e5bcf59b3b03c16
adam <spencerstingley6@gmail.com>
2023-10-19 10:29:04 -0500

    Fixed packer install to telescope installs correctly

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -3,12 +3,14 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }	
+    -- use {
+    --    'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    --     requires = { {'nvim-lua/plenary.nvim'} }
+    -- }	
 
+    use{'nvim-telescope/telescope.nvim', 
+        requires = {'nvim-lua/plenary.nvim'}
+    }
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
